85ecc753051290c6f4092884b692246ce4fa9486
Kyle Bashour <kylebshr@me.com>
2016-01-24 02:06:33 -0500

    Add a game play view controller class

---
+++ Dilemma/GamePlayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GamePlayViewController.swift
+//  Dilemma
+//
+//  Created by Kyle Bashour on 1/24/16.
+//  Copyright © 2016 Kyle Bashour. All rights reserved.
+//
+
+import UIKit
+
+class GamePlayViewController: UIViewController, GameInterfaceUpdatable {
+    
+    func gameManagerUpdateCards(gameManager: GameManager) {
+        
+    }
+    
+    func gameManagerUpdateCurrentPlayersTurn(gameManager: GameManager) {
+        
+    }
+}
