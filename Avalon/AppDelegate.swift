4b2e99087c94d71d0d7b349427048ebe20ac041d
Kyle Bashour <kylebshr@me.com>
2016-01-26 17:06:07 -0500

    Save a theme on open

--- Avalon/AppDelegate.swift
+++ Avalon/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        NSUserDefaults.standardUserDefaults().setObject("Dark", forKey: Constants.Defaults.theme)
+        
         return true
     }
 
