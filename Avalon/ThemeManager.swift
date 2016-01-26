97faf35bd0707fc28328a6a70763ef19779256c0
Kyle Bashour <kylebshr@me.com>
2016-01-26 17:04:31 -0500

    Add some colors to theme manager

--- Avalon/ThemeManager.swift
+++ Avalon/ThemeManager.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2016 Kyle Bashour. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ThemeManager {
+    
+    static var theme: Theme {
+        let savedTheme = NSUserDefaults.standardUserDefaults().stringForKey(Constants.Defaults.theme) ?? ""
+        return Theme(rawValue: savedTheme) ?? .Light
+    }
+    
+    static var mainBackgroundColor: UIColor {
+        switch theme {
+        case .Dark: return .flatBlackColorDark()
+        case .Plum: return .flatPlumColorDark()
+        default: return .whiteColor()
+        }
+    }
+    
+    static var currentMissionColor: UIColor {
+        switch theme {
+        case .Dark: return .flatBlackColor()
+        case .Plum: return .flatPlumColor()
+        default: return .lightGrayColor()
+        }
+    }
+    
+    static var failedMissionColor: UIColor {
+        return .flatRedColor()
+    }
+    
+    static var successfulMissionColor: UIColor {
+        return .flatGreenColor()
+    }
+}
+
+enum Theme: String {
+    case Dark
+    case Light
+    case Plum
+}
