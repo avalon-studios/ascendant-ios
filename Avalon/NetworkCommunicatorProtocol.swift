c362c26eb0f221d77aed6a441dd1a8b66f7ec22b
Kyle Bashour <kylebshr@me.com>
2016-01-25 20:19:42 -0500

    Fix missing file

---
+++ Avalon/NetworkCommunicatorProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkCommunicatorProtocol.swift
+//  Avalon
+//
+//  Created by Kyle Bashour on 1/25/16.
+//  Copyright © 2016 Kyle Bashour. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkCommunicatorProtocol: class {
+    func proposeMission(players: [Player])
+}
